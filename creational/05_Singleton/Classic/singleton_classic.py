3ebd88d6753eba876dd5ab74aec22b28c355d11f
andrealai <lai.andrea75@gmail.com>
2023-12-04 19:36:12 +0000

    Singleton Classic

---
+++ creational/05_Singleton/Classic/singleton_classic.py
@@ -0,0 +1,30 @@
+import datetime
+
+class Singleton(object):
+    ans = None
+    
+    @staticmethod
+    def instance():
+        if '_instance' not in Singleton.__dict__:
+            Singleton._instance = Singleton()
+        return Singleton._instance
+    
+    def open_log(self, path):
+        self.log_file = open(path, mode='w')
+        
+    def write_log(self, log_record):
+        now = str(datetime.datetime.now())
+        record = f"{now}: {log_record}\n"
+        self.log_file.write(record)
+        
+    def close_log(self):
+        self.log_file.close()
+    
+logger = Singleton.instance()
+logger.open_log('my.log')
+logger.write_log('Logging with classic Singleton patter.')
+logger.close_log()
+
+with open('my.log', 'r') as f:
+    for line in f:
+        print(line, end='')
